68da0c0380aac9616bcb0706d17bfc9911483c48
andywar65 <andy.war1965@gmail.com>
2024-07-06 17:21:30 +0200

    added redirect view

--- djaframe/urls.py
+++ djaframe/urls.py
@@ -19,9 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(pattern_name="entities:entity_list")),
     path("3D/", include("entities.urls", namespace="entities")),
 ]
 
