7017b0f2e0570380ee645d8b4a6823d2e236ad83
andywar65 <andy.war1965@gmail.com>
2024-07-07 11:16:03 +0200

    copy app files

---
+++ admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Entity
+
+
+@admin.register(Entity)
+class EntityAdmin(admin.ModelAdmin):
+    list_display = ("title", "description")
---
+++ urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from .views import (
+    EntityCreateView,
+    EntityDetailView,
+    EntityListView,
+    EntityUpdateView,
+    entity_delete,
+    material_image_create,
+    material_image_delete,
+)
+
+app_name = "entities"
+urlpatterns = [
+    path("entity/all/", EntityListView.as_view(), name="entity_list"),
+    path("entity/add/", EntityCreateView.as_view(), name="entity_create"),
+    path("entity/<pk>/", EntityDetailView.as_view(), name="entity_detail"),
+    path("entity/<pk>/update/", EntityUpdateView.as_view(), name="entity_update"),
+    path("entity/<pk>/delete/", entity_delete, name="entity_delete"),
+    path("entity/<pk>/matimg-add/", material_image_create, name="matimg_create"),
+    path("matimg/<pk>/", material_image_delete, name="matimg_delete"),
+]
